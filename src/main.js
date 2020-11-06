46c6d482de181bfc4aade993169676d22cf3c636
kooler62 <kooler62@gmail.com>
2020-11-06 16:21:44 +0200

    компоненты и vue файлы

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 // import App from './App.vue'
 
 //регестрируем компонент
-Vue.component('my-counter', {
+var component = {
   template: `
     <div>
       <h2>Счетчик {{ counter }}</h2>
@@ -19,13 +19,29 @@ Vue.component('my-counter', {
       this.counter++;
     }
   }
-});
+};
 
 new Vue({
   el: '#app',
+  // регестрируем компонент локально
+  components:{
+    'my-counter': component
+  }
   // data: {
   //   counter: 0,
   //   counter2: 0
+  // }
+  //render: h => h(App)
+});
+
+new Vue({
+  el: '#app2',
+  components:{
+    'my-counter': component
+  }
+  // не работает
+  // components: {
+  //   myСounter: component
   // }
   //render: h => h(App)
 });

b1a768a37f0b9a5e7cd0996b9e4e4606c3c52755
kooler62 <kooler62@gmail.com>
2020-11-06 13:59:12 +0200

    dano

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: {
+    counter: 0,
+    counter2: 0
+  }
+  //render: h => h(App)
 })

bca44bf3b5fceecc24d352f0c516a685ed952a8c
kooler62 <kooler62@gmail.com>
2020-11-06 14:07:59 +0200

    реализация компонента

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import Vue from 'vue'
 // import App from './App.vue'
 
+//регестрируем компонент
+Vue.component('my-counter', {
+  template: `
+    <div>
+      <h2>Счетчик {{ counter }}</h2>
+      <button @click="counter ++">+1</button>
+    </div>
+  `,
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add: function () {
+      this.counter++;
+    }
+  }
+});
+
 new Vue({
   el: '#app',
-  data: {
-    counter: 0,
-    counter2: 0
-  }
+  // data: {
+  //   counter: 0,
+  //   counter2: 0
+  // }
   //render: h => h(App)
-})
+});

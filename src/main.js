163fd06c8e5eafb5716a01f06948c86920000f91
kooler62 <kooler62@gmail.com>
2020-11-08 20:59:31 +0200

    clear

--- src/main.js
+++ src/main.js
@@ -1,47 +1,8 @@
 import Vue from 'vue'
-// import App from './App.vue'
-
-//регестрируем компонент
-var component = {
-  template: `
-    <div>
-      <h2>Счетчик {{ counter }}</h2>
-      <button @click="counter ++">+1</button>
-    </div>
-  `,
-  data: function () {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    add: function () {
-      this.counter++;
-    }
-  }
-};
+ import App from './App.vue'
 
 new Vue({
   el: '#app',
-  // регестрируем компонент локально
-  components:{
-    'my-counter': component
-  }
-  // data: {
-  //   counter: 0,
-  //   counter2: 0
-  // }
-  //render: h => h(App)
+  render: h => h(App)
 });
 
-new Vue({
-  el: '#app2',
-  components:{
-    'my-counter': component
-  }
-  // не работает
-  // components: {
-  //   myСounter: component
-  // }
-  //render: h => h(App)
-});

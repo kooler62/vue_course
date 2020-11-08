40903f4fc7543f8f3441c72012cdc4094fc4c963
kooler62 <kooler62@gmail.com>
2020-11-08 21:12:46 +0200

    локальная подгрузка компонета

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,20 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue App'
+    //локальныий импорт компонента
+    import Pizza from "./Pizza.vue";
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue App'
+            }
+        },
+        components: {
+            'appPizza': Pizza
+            //или
+            // 'app-pizza': Pizza
+        }
     }
-  }
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-//Сначала импортруем компонент
-import Pizza from "./Pizza.vue";
 
-//создаем тег, чтобы не пересекался с html5
-Vue.component('app-pizza', Pizza);
 new Vue({
   el: '#app',
   render: h => h(App)

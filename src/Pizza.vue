493843ca9439dea9d539ba91cdc8e6195a75ae81
kooler62 <kooler62@gmail.com>
2020-11-08 21:38:22 +0200

    валидация входящих данныч через пропсы

--- src/Pizza.vue
+++ src/Pizza.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    <h2>Pizza name {{ pizzaName}}</h2>
+    <h2>Pizza name {{ pizzaName }}/ {{ reversName }}</h2>
     <p>price : {{ pizzaPrice }}</p>
   </div>
 </template>
 
 <script>
     export default {
-        props: ['pizzaName', 'pizzaPrice'],
+        // props: ['pizzaName', 'pizzaPrice'],
+        // props: {
+        //     pizzaName: String,
+        //     pizzaPrice: Number
+        // },
+        props: {
+            pizzaName: {
+                type: String,
+           //     required: true,
+                default: 'Empty'
+            },
+            pizzaPrice: Number
+        },
+        computed: {
+            reversName() {
+                return this.pizzaName.split('').reverse().join('');
+            }
+        }
     }
 </script>
 

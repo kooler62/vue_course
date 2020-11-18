f1e5d8334dc3b633d3f74bb2d6024b4b4cf799d6
kooler62 <kooler62@gmail.com>
2020-11-18 14:35:46 +0200

    настроили навбар, но с обновлением

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="/pizzas">Pizzas</a>
           </li>
         </ul>
       </div>

35b8102fc74f9cf247e5afd028e5693a1fbf764f
kooler62 <kooler62@gmail.com>
2020-11-18 14:32:50 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>

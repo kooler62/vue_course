24b15aabb64aeab9154d89289fdd2e294e736bd5
kooler62 <kooler62@gmail.com>
2020-11-18 13:56:18 +0200

    подключим роут файл

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import router from "./routes";
 
 Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  // router: router  или router если совпадает ключ, значение
+  router
 });

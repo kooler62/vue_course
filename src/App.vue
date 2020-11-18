dcf0e848f2f6364d02030e04ddf314a93a334347
kooler62 <kooler62@gmail.com>
2020-11-18 14:50:23 +0200

    другой способ router-link

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,22 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
+<!--          <li class="nav-item">-->
+<!--            <router-link class="nav-link" to="/">Home</router-link>-->
+<!--          </li>-->
 <!--          или байдим через :to -->
-          <li class="nav-item">
-            <router-link class="nav-link" :to="'/pizzas'">Pizzas</router-link>
-          </li>
+<!--          <li class="nav-item active">-->
+<!--            <router-link class="nav-link" :to="'/pizzas'">Pizzas</router-link>-->
+<!--          </li>-->
+
+
+          <!--    active-class="active" какой клас добавлять активной ссылке, tag показывает каким тегом обернуть ссылку      -->
+          <router-link tag="li" class="nav-item" :to="'/'">
+            <a class="nav-link" active-class="active">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" :to="'/pizzas'">
+            <a class="nav-link" active-class="active">Pizzas</a>
+          </router-link>
         </ul>
       </div>
     </nav>
